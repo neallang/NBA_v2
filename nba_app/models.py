25245c95de097f9c0295d351ccaf0fc49d737794
Neal Langhorne <neal_langhorne@yahoo.com>
2024-06-04 16:12:57 -0400

    barebones template for the app. can enter players names and will see their team and position

--- nba_app/models.py
+++ nba_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Player(models.Model):
+    player_id = models.IntegerField(unique=True)
+    full_name = models.CharField(max_length=100)
+    team = models.CharField(max_length=100)
+    position = models.CharField(max_length=10)
+    height = models.CharField(max_length=10)
+    weight = models.CharField(max_length=10)
+    college = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    draft_year = models.IntegerField(null=True, blank=True)
+    draft_round = models.IntegerField(null=True, blank=True)
+    draft_number = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.full_name
--- nba_app/views.py
+++ nba_app/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .forms import PlayerSearchForm
+from nba_api.stats.static import players
+from nba_api.stats.endpoints import commonplayerinfo
 
-# Create your views here.
+def get_player_info(player_name):
+    player_dict = players.get_players()
+    player = next((p for p in player_dict if p['full_name'].lower() == player_name.lower()), None)
+    if player:
+        player_info = commonplayerinfo.CommonPlayerInfo(player_id=player['id'])
+        return player_info.get_normalized_dict()
+    return None
+
+def compare_players(request):
+    form = PlayerSearchForm()
+    player1_info = player2_info = None
+
+    if request.method == 'POST':
+        form = PlayerSearchForm(request.POST)
+        if form.is_valid():
+            player1_name = form.cleaned_data['player1']
+            player2_name = form.cleaned_data['player2']
+            player1_info = get_player_info(player1_name)
+            player2_info = get_player_info(player2_name)
+
+    context = {
+        'form': form,
+        'player1_info': player1_info,
+        'player2_info': player2_info,
+    }
+    return render(request, 'nba_app/compare_players.html', context)
