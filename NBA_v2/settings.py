6713f691e84ae8c95fcf9ec93b16c1a39405e721
Neal Langhorne <neal_langhorne@yahoo.com>
2024-06-09 14:14:38 -0400

    separate js file. information function (on top of stat function).

--- NBA_v2/settings.py
+++ NBA_v2/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
--- nba_app/views.py
+++ nba_app/views.py
@@ -4,17 +4,25 @@ from .forms import PlayerSearchForm
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import commonplayerinfo, playercareerstats
 
-def get_player_info(player_name):
+# retrieves the corresponding statistics for a given player
+def get_player_stats(player_name):
     player_dict = players.get_players()
     player = next((p for p in player_dict if p['full_name'].lower() == player_name.lower()), None)
     if player:
         player_info = commonplayerinfo.CommonPlayerInfo(player_id=player['id']).get_normalized_dict()
         career_stats = playercareerstats.PlayerCareerStats(player_id=player['id']).get_normalized_dict()
         career_totals = career_stats['CareerTotalsRegularSeason'][0]
+
         career_ppg = career_totals['PTS']
         career_apg = career_totals['AST']
         career_rpg = career_totals['REB']
+        career_bpg = career_totals['BLK']
+        career_spg = career_totals['STL']
+        career_fg_pct = career_totals['FG_PCT'] * 100
+        career_3p_pct = career_totals['FG3_PCT'] * 100
+        career_ft_pct = career_totals['FT_PCT'] * 100
         games_played = career_totals['GP']
+
         if games_played > 0:
             career_ppg /= games_played
             career_ppg = round(career_ppg, 1)
@@ -24,16 +32,49 @@ def get_player_info(player_name):
 
             career_rpg /= games_played
             career_rpg = round(career_rpg, 1)
+
+            career_bpg /= games_played
+            career_bpg = round(career_bpg, 1)
+
+            career_spg /= games_played
+            career_spg = round(career_spg, 1)
+
         return {
-            'player_info': player_info,
             'career_ppg': career_ppg,
             'career_apg': career_apg,
-            'career_rpg': career_rpg
+            'career_rpg': career_rpg,
+            'career_bpg': career_bpg,
+            'career_spg': career_spg,
+            'career_fg_pct': round(career_fg_pct, 1),
+            'career_3p_pct': round(career_3p_pct, 1),
+            'career_ft_pct': round(career_ft_pct, 1),
         }
     return None
 
+# retrieves the corresponding personal information for a given player
+def get_player_info(player_name):
+    player_dict = players.get_players()
+    player = next((p for p in player_dict if p['full_name'].lower() == player_name.lower()), None)
+    if player:
+        player_info = commonplayerinfo.CommonPlayerInfo(player_id=player['id']).get_normalized_dict()
+        common_info = player_info['CommonPlayerInfo'][0]
+
+        return {
+            'full_name': common_info['DISPLAY_FIRST_LAST'],
+            'birth_date': common_info['BIRTHDATE'],
+            'height': common_info['HEIGHT'],
+            'weight': common_info['WEIGHT'],
+            'college': common_info['SCHOOL'],
+            'country': common_info['COUNTRY'],
+            'draft_year': common_info['DRAFT_YEAR'],
+            'draft_round': common_info['DRAFT_ROUND'],
+            'draft_number': common_info['DRAFT_NUMBER'],
+        }
+    return None
+
+# calls other functions to retrieve information and passes to template
 def compare_players(request):
-    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':  # check if we have an ajax request
         query = request.GET.get('q', '')
         player_dict = players.get_players()
         filtered_players = [player for player in player_dict if query.lower() in player['full_name'].lower()]
@@ -41,6 +82,7 @@ def compare_players(request):
 
     form = PlayerSearchForm()
     player1_info = player2_info = None
+    player1_stats = player2_stats = None
 
     if request.method == 'POST':
         form = PlayerSearchForm(request.POST)
@@ -49,10 +91,14 @@ def compare_players(request):
             player2_name = form.cleaned_data['player2']
             player1_info = get_player_info(player1_name)
             player2_info = get_player_info(player2_name)
+            player1_stats = get_player_stats(player1_name)
+            player2_stats = get_player_stats(player2_name)
 
     context = {
         'form': form,
         'player1_info': player1_info,
         'player2_info': player2_info,
+        'player1_stats': player1_stats,
+        'player2_stats': player2_stats,
     }
     return render(request, 'nba_app/compare_players.html', context)
