77ee7b545befd00f91c5e52917c82e4a5d4d3ece
Neal Langhorne <neal_langhorne@yahoo.com>
2024-06-25 17:40:46 -0400

    all HOF available

---
+++ nba_app/utils/hof_since_19.py
@@ -0,0 +1,14 @@
+hall_of_fame = {
+    "2019": ["Vlade Divac", "Sidney Moncrief", "Jack Sikma", "Paul Westphal", "Bobby Jones", "Carl Braun", "Chuck Cooper"],
+    "2020": ["Kobe Bryant", "Tim Duncan", "Kevin Garnett", "Rudy Tomjanovich"],
+    "2021": ["Chris Bosh", "Paul Pierce", "Ben Wallace", "Chris Webber", "Toni Kukoč", "Bob Dandridge"],
+    "2022": ["Manu Ginóbili", "Tim Hardaway", "Swin Cash"],
+    "2023": ["Dwyane Wade", "Dirk Nowitzki", "Pau Gasol", "Tony Parker"],
+    "2024": ["Carmelo Anthony", "Dwight Howard", "Vince Carter", "Amar'e Stoudemire"]
+}
+
+def is_hall_of_famer(player_name):
+    for year, players in hall_of_fame.items():
+        if player_name in players:
+            return True
+    return False
