622ff0a157f08db33f32b0a5a7c2c79df9070340
Neal Langhorne <neal_langhorne@yahoo.com>
2024-06-25 16:12:17 -0400

    awards tab working, information inaccurate. switched to VSCode

--- nba_app/static/js/toggleTables.js
+++ nba_app/static/js/toggleTables.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
+    $("#career-stats-table").show();
+    $("#personal-info-table").hide();
+    $("#awards-table").hide();
+    
     $("#toggle-stats").click(function() {
         $("#personal-info-table").hide();
         $("#career-stats-table").show();
+        $("#awards-table").hide();
     });
     $("#toggle-info").click(function() {
         $("#career-stats-table").hide();
         $("#personal-info-table").show();
+        $("#awards-table").hide();
+    });
+    $("#toggle-awards").click(function() {
+        $("#career-stats-table").hide();
+        $("#personal-info-table").hide();
+        $("#awards-table").show();
+
     });
 });
