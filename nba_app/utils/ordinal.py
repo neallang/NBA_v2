0ba3f1f2baf3dd5af66e997ce0814312cefc2a12
Neal Langhorne <neal_langhorne@yahoo.com>
2024-06-27 14:26:26 -0400

    stark refactoring to reduce API calls and increase organization. REB safeguarding

---
+++ nba_app/utils/ordinal.py
@@ -0,0 +1,10 @@
+# Function to format ordinal numbers (1st, 2nd, 3rd) - formatting 
+def ordinal(n):
+    if n == "Undrafted":
+        return ""
+    n = int(n)
+    if 11 <= (n % 100) <= 13:
+        suffix = 'th'
+    else:
+        suffix = {1: 'st', 2: 'nd', 3: 'rd'}.get(n % 10, 'th')
+    return str(n) + suffix
