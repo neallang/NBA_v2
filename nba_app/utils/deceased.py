43eae443109505da449a404f679c1c1e8ca7951b
Neal Langhorne <neal_langhorne@yahoo.com>
2024-06-27 14:51:13 -0400

    added cache to deceased.py

--- nba_app/utils/deceased.py
+++ nba_app/utils/deceased.py
@@ -1,8 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from django.core.cache import cache
 
 def is_deceased(player_name):
+    cache_key = f"deceased_{player_name.lower().replace(' ', '_')}"
+    cached_result = cache.get(cache_key)
+    if cached_result:
+        return cached_result
+
     search_url = f"https://en.wikipedia.org/wiki/{player_name.replace(' ', '_')}"
     response = requests.get(search_url)
     if response.status_code == 200:
@@ -16,15 +22,17 @@ def is_deceased(player_name):
                     if death_info:
                         death_text = death_info.text.strip()
                         # Extract death date
-                        death_date_match = re.search(r'([A-Za-z]+ \d{1,2}, \d{4})', death_text)
+                        death_date_match = re.search(r'([A-Za-z]+\s+\d{1,2},\s+\d{4}|[0-9]+\s+[A-Za-z]+\s+\d{4})', death_text)
                         death_date = death_date_match.group(1) if death_date_match else "Unknown"
 
                         # Extract age at death
-                        age_at_death_match = re.search(r'\(?aged\s*(\d+)\)?', death_text, re.IGNORECASE) # finally cracked this - how to find age
+                        age_at_death_match = re.search(r'\(aged\s*(\d+)\)', death_text, re.IGNORECASE)
                         age_at_death = age_at_death_match.group(1) if age_at_death_match else "Unknown"
 
-                        return True, death_date, age_at_death
+                        result = (True, death_date, age_at_death)
+                        cache.set(cache_key, result, timeout=60*60*24) 
+                        return result
 
-
-
-    return False, None, None
+    result = (False, None, None)
+    cache.set(cache_key, result, timeout=60*60*24)  # 24 hours
+    return result
